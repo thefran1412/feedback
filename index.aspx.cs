799e7ed5b0041e9ed30d1f70ec33d7950c36329b
Francesc Edo Trias <francesc.edo.trias@gmail.com>
2017-05-05 16:38:14 +0200

    class database

--- index.aspx.cs
+++ index.aspx.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Data;
+using System.Data.SqlClient;
+using System.Configuration;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -9,11 +11,10 @@ using System.Web.UI.WebControls;
 public partial class index : System.Web.UI.Page
 {
     protected void Page_Load(object sender, EventArgs e)
-    {
-        string query = "select * from users";
+    {   
+        sqlCommand cmd = 
         dadesNico.Class1 dades = new dadesNico.Class1();
-        DataSet hola = dades.downloadData(query);
-
-        var adios = hola.Tables["dades"];
+        DataSet ds = dades.downloadData("select * from users");
+        SqlDataAdapter da = new SqlDataAdapter()
     }
 }
