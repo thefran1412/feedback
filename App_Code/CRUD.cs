e0f9e039d1d3c386bfb0a978330618a3ab256c70
Francesc Edo Trias <francesc.edo.trias@gmail.com>
2017-05-15 20:35:12 +0200

    some changes 2

--- App_Code/CRUD.cs
+++ App_Code/CRUD.cs
@@ -41,7 +41,19 @@ namespace crud
         {
             value = value + DateTime.Now.ToString("yyyyMMddHHmmssfff");
 
+            public static byte[] GetHash(string inputString)
+            {
+                HashAlgorithm algorithm = MD5.Create();  //or use SHA1.Create();
+                return algorithm.ComputeHash(Encoding.UTF8.GetBytes(inputString));
+            }
 
+            var inputString = "hola";
+                StringBuilder sb = new StringBuilder();
+                foreach (byte b in GetHash(inputString))
+                    sb.Append(b.ToString("X2"));
+
+                return sb.ToString();
+            
 
             return value.GetHashCode().ToString();
         }
