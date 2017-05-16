fd04e2c525ec83863d403643a13353f42546962a
Francesc Edo Trias <francesc.edo.trias@gmail.com>
2017-05-16 03:15:48 +0200

    crud form v2: delete / view

--- form/view.aspx.cs
+++ form/view.aspx.cs
@@ -13,23 +13,18 @@ public partial class form_view : System.Web.UI.Page
     protected void Page_Load(object sender, EventArgs e)
     {
         // getting variable from url
-        var hash = Page.RouteData.Values["hash"];
-        var query = "";
-
-        // first query
-        query = "SELECT * FROM forms WHERE hash = '" + hash + "'";
-        Base conn = new Base();
-        DataSet first = conn.getData(query);
+        var hash = Page.RouteData.Values["hash"].ToString();
 
         // if there's an entry stay in page and continue, else go to page before this
         Permissions p = new Permissions();
         p.set();
-        p.accessForm(hash.ToString());
 
+        Forms form = new Forms();
+        DataSet first = form.getInfo(hash);
+        
         // set info to variable
         var data = first.Tables[0].Rows[0].ItemArray;
         name.Text = data[2].ToString();
 
- 
     }
 }
