da86e7dbe0a13fa01b0fb2db8875c35821f0970c
Francesc Edo Trias <francesc.edo.trias@gmail.com>
2017-05-17 16:53:15 +0200

    landing done

--- index.aspx
+++ index.aspx
@@ -6,34 +6,17 @@
 
 <asp:Content ID="contentInside" ContentPlaceHolderID="content" Runat="Server">
         <div class="title">
-        <a class="back" href="/folder/index/">
-            <img src="/img/back.png"/>
-        </a>
-        <asp:Label ID="title" runat="server" Text="FOLDER"></asp:Label>
-        <div class="actions">
-            <a id="edit" runat="server" href="/folder/edit/">Edit</a>
-            <a id="delete" runat="server" href="/folder/delete/">Delete</a>
-        </div>
+        <asp:Label ID="title" runat="server" Text="SOME PUBLIC FORMS"></asp:Label>
     </div>
     <asp:Repeater ID="Repeater1" runat="server">
         <ItemTemplate>
-            
+            <a class="folderUrl" href="/form/view/<%#Eval("hash") %>">
                 <div class="folder" style="background: linear-gradient(to right, <%#Eval("color1") %> , <%#Eval("color2") %>);">
-                    <a class="folderUrl" href="/form/view/<%#Eval("hash") %>">
+                    
                         <asp:Label ID="data" runat="server" Text='<%#Eval("name") %>'></asp:Label>
-                    </a>
-                    <div class="onSite">
-                        <a href="/form/edit/<%#Eval("hash") %>">Edit</a>
-                        <a href="/form/delete/<%#Eval("hash") %>">Delete</a>
-                    </div>
-
                 </div>
+            </a>
 
         </ItemTemplate>
     </asp:Repeater>
-    <a class="folderUrl" ID="add_new" runat="server" href="/form/add/">
-        <div class="folder add">    
-            <span>+</span>
-        </div>
-    </a>
 </asp:Content>
--- index.aspx.cs
+++ index.aspx.cs
@@ -7,48 +7,17 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using db;
 
 public partial class index : System.Web.UI.Page
 {
     protected void Page_Load(object sender, EventArgs e)
     {
-        /*DataBase obj = new DataBase();
-        obj.getData("select * from users");
-        Repeater1.DataSource() = obj;
-        Repeater1.DataBind();
-
-        var adios = hola.Tables["dades"];*/
-        //ViewState["GoBackTo"] = Request.UrlReferrer;
-
-        //if(Session["userId"].ToString() != "")
-        //{
-        //    Response.Redirect("/folder/index");
-        //}
-        //Session["url"] = Request.Url.ToString();
-        if (Session["userId"] != null && Session["userId"].ToString() != "")
-        {
-            Response.Redirect("/folder/index");
-        }
         Session["url"] = Request.Url.ToString();
 
-
-        if (Page.RouteData.Values["hash"] == null)
-        {
-            Permissions.goBack();
-        }
-
-        // getting variable from url
-        var hash = Page.RouteData.Values["hash"].ToString();
-        add_new.HRef = "/form/add/" + hash;
-        edit.HRef = "/folder/edit/" + hash;
-        delete.HRef = "/folder/delete/" + hash;
-        // if there's an entry stay in page and continue, else go to page before this
-        Permissions p = new Permissions();
-        p.set();
-
-        // get objects
-        Folders folder = new Folders();
-        DataSet ds = folder.getForms(hash);
+        var query = "SELECT * FROM forms WHERE visible = 1";
+        Base db = new Base();
+        DataSet ds = db.getData(query);
 
         // send objects data to the repeater
         Repeater1.DataSource = ds;
